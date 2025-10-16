cd406c45617dc6e4776b29c17a79b99ec912c1ea
aaronjsharp <aaron.sharp2011@gmail.com>
2025-10-16 12:46:11 -0500

    sentry

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,40 +1,76 @@
-import prisma from "@/lib/db";
-import { inngest } from "./client";
-import { createOpenAI } from "@ai-sdk/openai";
-import { createGoogleGenerativeAI } from "@ai-sdk/google"
-import { createAnthropic } from "@ai-sdk/anthropic"
-import { generateText } from "ai";
+import prisma from '@/lib/db';
+import { inngest } from './client';
+import { createOpenAI } from '@ai-sdk/openai';
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
+import { createAnthropic } from '@ai-sdk/anthropic';
+import { generateText } from 'ai';
+import * as Sentry from "@sentry/nextjs"
 
-const openai = createOpenAI()
-const gemini = createGoogleGenerativeAI()
-const anthropic = createAnthropic()
+const openai = createOpenAI();
+const gemini = createGoogleGenerativeAI();
+const anthropic = createAnthropic();
 
 export const execute = inngest.createFunction(
-  { id: "execute-ai" },
-  { event: "execute/ai" }, async ({ event, step }) => {
-    await step.sleep("pretend-to-sleep", "5s")
-    const { steps: openaiSteps } = await step.ai.wrap("openai-generate-text", generateText, {
-      model: openai("gpt-5"),
-      system: "You are a helpful assistant.",
-      prompt: "What is 2 + 2"
-    });
+  { id: 'execute-ai' },
+  { event: 'execute/ai' },
+  async ({ event, step }) => {
+    await step.sleep('pretend-to-sleep', '5s');
 
-    const { steps: geminiSteps } = await step.ai.wrap("gemini-generate-text", generateText, {
-      model: gemini("gemini-2.5-flash"),
-      system: "You are a helpful assistant.",
-      prompt: "What is 2 + 2"
+    Sentry.logger.info("User triggered test log", {
+      log_source: 'sentry_test'
     });
+    console.warn("Something is missing");
+    console.error("This is an error I want to track")
+    
+    const { steps: openaiSteps } = await step.ai.wrap(
+      'openai-generate-text',
+      generateText,
+      {
+        model: openai('gpt-5'),
+        system: 'You are a helpful assistant.',
+        prompt: 'What is 2 + 2',
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      }
+    );
 
-    const { steps: anthropicSteps } = await step.ai.wrap("anthropic-generate-text", generateText, {
-      model: anthropic("claude-sonnet-4-5"),
-      system: "You are a helpful assistant.",
-      prompt: "What is 2 + 2"
-    });
+    const { steps: geminiSteps } = await step.ai.wrap(
+      'gemini-generate-text',
+      generateText,
+      {
+        model: gemini('gemini-2.5-flash'),
+        system: 'You are a helpful assistant.',
+        prompt: 'What is 2 + 2',
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      }
+    );
+
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      'anthropic-generate-text',
+      generateText,
+      {
+        model: anthropic('claude-sonnet-4-5'),
+        system: 'You are a helpful assistant.',
+        prompt: 'What is 2 + 2',
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      }
+    );
 
     return {
       openaiSteps,
       geminiSteps,
-      anthropicSteps
-    }
-  }  
+      anthropicSteps,
+    };
+  }
 );
