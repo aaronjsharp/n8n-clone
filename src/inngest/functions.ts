acd2f152ed92f0dce08fd1558787a5c739d37bfa
aaronjsharp <aaron.sharp2011@gmail.com>
2025-10-16 11:31:16 -0500

    add ai providers

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,19 +1,40 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import { createOpenAI } from "@ai-sdk/openai";
+import { createGoogleGenerativeAI } from "@ai-sdk/google"
+import { createAnthropic } from "@ai-sdk/anthropic"
+import { generateText } from "ai";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("fetching", "5s");
-    await step.sleep("transcribing", "5s");
-    await step.sleep("summarazing", "5s");
-    await step.run("create-workflow", () => {
-      return prisma.workflow.create({
-        data: {
-          name: "workflow-from-inngest"
-        }
-      })
-    })
-  },
+const openai = createOpenAI()
+const gemini = createGoogleGenerativeAI()
+const anthropic = createAnthropic()
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute/ai" }, async ({ event, step }) => {
+    await step.sleep("pretend-to-sleep", "5s")
+    const { steps: openaiSteps } = await step.ai.wrap("openai-generate-text", generateText, {
+      model: openai("gpt-5"),
+      system: "You are a helpful assistant.",
+      prompt: "What is 2 + 2"
+    });
+
+    const { steps: geminiSteps } = await step.ai.wrap("gemini-generate-text", generateText, {
+      model: gemini("gemini-2.5-flash"),
+      system: "You are a helpful assistant.",
+      prompt: "What is 2 + 2"
+    });
+
+    const { steps: anthropicSteps } = await step.ai.wrap("anthropic-generate-text", generateText, {
+      model: anthropic("claude-sonnet-4-5"),
+      system: "You are a helpful assistant.",
+      prompt: "What is 2 + 2"
+    });
+
+    return {
+      openaiSteps,
+      geminiSteps,
+      anthropicSteps
+    }
+  }  
 );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,20 +1,29 @@
 import { inngest } from '@/inngest/client';
 import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
+import { openai } from '@ai-sdk/openai';
+import { generateText } from 'ai';
+
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai"
+    })
+
+    return { success: true, message: 'Job queued' };
+  }),
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
   createWorkflow: protectedProcedure.mutation(async () => {
     await inngest.send({
-      name: "test/hello.world",
+      name: 'test/hello.world',
       data: {
-        email: "aaron@mail.com"
-      }
+        email: 'aaron@mail.com',
+      },
     });
-    return { success: true, message: "Job queued"}
+    return { success: true, message: 'Job queued' };
   }),
-
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
