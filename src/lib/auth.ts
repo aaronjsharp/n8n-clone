6e72ff16db483e78f0244936a5dc1265f4aac771
aaronjsharp <aaron.sharp2011@gmail.com>
2025-10-16 16:20:58 -0500

    Polar for payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,35 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
+import { checkout, polar, portal } from '@polar-sh/better-auth';
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
 // If your Prisma file is located elsewhere, you can change the path
-import prisma from "@/lib/db";
+import prisma from '@/lib/db';
+import { polarClient } from './polar';
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: '42174602-0d87-4c91-ab85-6c45c068b9fb',
+              slug: 'pro',
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true
+        }),
+        portal()
+      ],
     }),
-    emailAndPassword: {
-      enabled: true,
-      autoSignIn: true,
-    },
+  ],
 });
