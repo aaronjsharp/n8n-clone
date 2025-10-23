669a35ff0a717503c2064074beaf519f2e968633
aaronjsharp <aaron.sharp2011@gmail.com>
2025-10-23 14:15:36 -0500

    node selector

---
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -0,0 +1,19 @@
+import { NodeProps } from "@xyflow/react";
+import { BaseTriggerNode } from "../base-trigger-node";
+import { MousePointerIcon } from "lucide-react";
+import { memo } from "react";
+
+export const ManualTriggerNode = memo((props: NodeProps) => {
+  return (
+    <>
+      <BaseTriggerNode 
+        {...props}
+        icon={MousePointerIcon}
+        name="Clicking will 'Execute workflow'"
+        // status={nodeStatus}
+        // onSettings={handleOpenSettings}
+        // onDoubleClick={handleOpenSettings}
+      />
+    </>
+  )
+})
