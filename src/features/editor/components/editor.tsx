3916f35f8d7fc0dcf2d6cd5ab807b95f52eb7ced
aaronjsharp <aaron.sharp2011@gmail.com>
2025-11-21 14:47:13 -0600

    node execution

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, type Node, type Edge, type NodeChange, type EdgeChange, type Connection, Background, Controls, MiniMap, Panel } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
@@ -10,6 +10,8 @@ import { nodeComponents } from '@/config/node-components';
 import { AddNodeButton } from './add-node-button';
 import { useSetAtom } from 'jotai';
 import { editorAtom } from '../store/atoms';
+import { NodeType } from '@/generated/prisma';
+import { ExecuteWorkflowButton } from './execute-workflow-button';
 
 export const EditorLoading = () => {
   return <LoadingView message="Loading editor..." />
@@ -40,6 +42,10 @@ export const Editor = ({ workflowId }: { workflowId: string }) => {
     [],
   );
 
+  const hasManualTrigger = useMemo(() => {
+    return nodes.some((node) => node.type === NodeType.MANUAL_TRIGGER)
+  }, [nodes])
+
   return (
     <div className='size-full'>
       <ReactFlow
@@ -66,6 +72,11 @@ export const Editor = ({ workflowId }: { workflowId: string }) => {
         <Panel position='top-right'>
           <AddNodeButton />
         </Panel>
+        {hasManualTrigger && (
+          <Panel position='bottom-center'>
+            <ExecuteWorkflowButton workflowId={workflowId} />
+        </Panel>
+        )}
       </ReactFlow>
     </div>
   )
